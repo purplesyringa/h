e6ac28322451ff7da7f2fdf74c662cb78440ad2d
Alisa Sireneva <me@purplesyringa.moe>
2024-12-07 11:18:21 +0300

    Split out `h` types into `h-bare`

---
+++ h-bare/src/lib.rs
@@ -0,0 +1,33 @@
+//! This crate provides types for [`h`](https://docs.rs/h). Use that crate instead.
+
+#![no_std]
+#![cfg_attr(doc, feature(doc_auto_cfg))]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+pub mod codegen;
+mod const_vec;
+pub mod hash;
+mod map;
+mod phf;
+pub(crate) mod scatter;
+mod set;
+pub(crate) mod unhashed_phf;
+
+pub use map::Map;
+pub use phf::Phf;
+pub use set::Set;
+
+#[doc(hidden)]
+pub mod low_level {
+    pub use super::const_vec::ConstVec;
+    pub use super::unhashed_phf::{Mixer, UnhashedPhf};
+}
+
+#[cfg(test)]
+mod tests;
--- h/src/lib.rs
+++ h/src/lib.rs
@@ -39,33 +39,6 @@
 //! - `serde`: Enables [serde](https://serde.rs/) support.
 
 #![no_std]
-#![cfg_attr(doc, feature(doc_auto_cfg))]
-#![deny(unsafe_op_in_unsafe_fn)]
 
-#[cfg(feature = "alloc")]
-extern crate alloc;
-
-#[cfg(feature = "std")]
-extern crate std;
-
-pub mod codegen;
-mod const_vec;
-pub mod hash;
-mod map;
-mod phf;
-pub(crate) mod scatter;
-mod set;
-pub(crate) mod unhashed_phf;
-
-pub use map::Map;
-pub use phf::Phf;
-pub use set::Set;
-
-#[doc(hidden)]
-pub mod low_level {
-    pub use super::const_vec::ConstVec;
-    pub use super::unhashed_phf::{Mixer, UnhashedPhf};
-}
-
-#[cfg(test)]
-mod tests;
+#[doc(inline)]
+pub use h_bare::*;
